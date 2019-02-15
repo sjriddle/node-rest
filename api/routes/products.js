0c2ff4d5483ee7326742cca19b211cd87cd2be69
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-02-15 10:03:10 -0700

    Update products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const router = express.Router();
+const express = require('express');
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 

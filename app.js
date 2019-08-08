67ce538a4949a783e2b524a2f1443f9fef6c5770
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-08-08 08:56:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
